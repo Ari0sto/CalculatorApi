c5199af36331fbec45430b03afa1c1a415aca65a
Roman <nechik.biz@gmail.com>
2025-12-17 19:03:12 +0200

    feat: added sum method

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("sum")]
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
